5ecc124d0d04c5c0beb58ecd6cc29eb459361e18
LAVIEユーザー <LAVIE@192.168.10.101>
2020-05-15 23:17:59 +0900

    さらに付け加えました

--- softfirst/src/softfirst/FavoriteFood.java
+++ softfirst/src/softfirst/FavoriteFood.java
@@ -4,7 +4,9 @@ public class FavoriteFood {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("My favorite food is オムライス");
+		System.out.println("私の好きな食べ物は、オムライスです。");
+		System.out.println("上から卵をのせるタイプのオムライスが好きで、ふわふわの卵が最高に美味しいです。");
+		System.out.println("ふわふわの卵と言えば、だし巻き卵や天津飯も好きです。");
 	}
 
 }
