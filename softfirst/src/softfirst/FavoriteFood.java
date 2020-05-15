05ba8e667ddf2331ff645e3dff01ae0fe35411a9
LAVIEユーザー <LAVIE@192.168.10.101>
2020-05-15 23:13:12 +0900

    理由を追加しました

---
+++ softfirst/src/softfirst/FavoriteFood.java
@@ -0,0 +1,10 @@
+package softfirst;
+
+public class FavoriteFood {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("My favorite food is オムライス");
+	}
+
+}
